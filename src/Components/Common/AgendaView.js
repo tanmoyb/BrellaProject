5daa2844113244286afd57dbe919335b10fe4032
Tanmoy <tanmoy.zadid@yahoo.com>
2021-03-07 15:32:50 +0200

    date table updated with props value

--- src/Components/Common/AgendaView.js
+++ src/Components/Common/AgendaView.js
@@ -23,26 +23,32 @@ const styles = () => ({
     minWidth: 650,
   },
 })
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+
+
 export class AgendaView extends React.Component{
     constructor(props) {
         super(props);
         
       }
 
-     
+      createData(name, calories, fat, carbs, protein) {
+        return { name, calories, fat, carbs, protein };
+      }
+
+      
       getAgendaTable(){
-        const { classes } = this.props;
+        const { classes,agendaTimeSlots } = this.props;
+
+        let rows = []
+        for (const [key, value] of Object.entries(agendaTimeSlots)) {
+          console.log(`${key}: ${value.attributes.title}`);
+          rows.push(this.createData(value.attributes.title,159, 6.0, 24, 4.0))
+        }
+        console.log("agendaTimes",agendaTimeSlots)
+        
+  
+
         return (
           <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="simple table">
--- src/Components/Containers/LandingPage.js
+++ src/Components/Containers/LandingPage.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import Axios from 'axios'
 import { withStyles } from '@material-ui/core/styles'
 import AgendaView from '../Common/AgendaView'
-
+import normalize from 'json-api-normalizer';
 
 const styles = () => ({
   root: {
@@ -21,16 +21,23 @@ export class LandingPage extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-          agenda: {}
+          agendaTimeSlots: []
         };
         
       }
   
   componentDidMount() {
-    Axios.get('https://api.brella.io/api/aalto/events/unicorndemo2025/time_slots').
-        then(res =>{
-            console.log("res",res)
-        })
+    fetch('https://api.brella.io/api/aalto/events/unicorndemo2025/time_slots')
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log("data",data)
+      let normalizedData = normalize(data)
+
+      console.log("normalized data",normalizedData)
+      this.setState({
+        agendaTimeSlots:normalizedData.timeSlots
+      })
+    })
         .catch(err =>{
             console.log(err)
         })
@@ -39,11 +46,10 @@ export class LandingPage extends React.Component{
 
   render() {
     const { classes } = this.props;
-
     return (
       <div className={classes.root}>
           Landing Page
-          <AgendaView></AgendaView>
+          <AgendaView agendaTimeSlots = {this.state.agendaTimeSlots}></AgendaView>
       </div>
     )
   }
